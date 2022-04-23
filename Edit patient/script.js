1f5e94649bf977e4b47eaf60475fe5036d5c8340
hassanchakkour <hassan.chakkour@hotmail.com>
2022-04-23 18:27:29 +0300

    Edit patient page

---
+++ Edit patient/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+        
+    $("button").click(function() {
+
+    $("h1, h2, p").toggleClass("blue");
+
+      $("p").css("background-color", "yellow");
+    
+});
+ });
