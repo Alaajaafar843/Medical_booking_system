20fbb11c6a327d73386e94daddffadb0b2361fb8
hassanchakkour <hassan.chakkour@hotmail.com>
2022-05-03 19:54:39 +0300

    responsive contact page

---
+++ Contact Page/main.js
@@ -0,0 +1,49 @@
+const form = document.getElementById('form');
+const uname = document.getElementById('name');
+const email = document.getElementById('email');
+const messages = document.getElementById('messages');
+const btn = document.getElementById('btn');
+
+{ 
+    function validation () { 
+        
+        if(uname.value === "") { 
+        uname.classList.add("error")
+        }
+        if(uname.value !== "") { 
+        uname.classList.remove("error")
+        uname.classList.add("success")
+        
+        }
+        if(email.value === "") { 
+        email.classList.add("error")
+        }
+        if(email.value !== "") { 
+        email.classList.remove("error")
+        }
+        
+        
+        if(messages.value === "") { 
+        messages.classList.add("error")
+        }
+        if(messages.value !== "") { 
+         messages.classList.remove("error")
+         messages.classList.add("success")
+        }
+        
+        
+    }
+    btn.addEventListener('click', validation)
+}
+{ 
+    
+}
+{ 
+    let input = document.getElementsByTagName('input')
+
+    function letterOnly (input) { 
+        var restric = /[^a-z]/gi ;
+        uname.value = input.value.replace(restric, "");
+    }
+    
+}
